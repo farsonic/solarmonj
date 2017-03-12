b7fa6402b3238b7cd8ca7ec5b56e60eaab397518
farsonic <fp@prowsehouse.com>
2017-03-12 14:43:38 +1000

    Update main.cpp
    Modified code for use with PVOutput

--- main.cpp
+++ main.cpp
@@ -4,60 +4,98 @@
 #include <string>
 #include <sstream>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/wait.h>
 #include <stdlib.h>
-
-#include "clientsocket.h"
-#include "socketexception.h"
 
 #include "jfyconnection.h"
 
 using namespace std;
 
-int main( int argc, char** argv )
-{
-	Jfy::Connection conn( "/dev/ttyUSB0" );
-	string logPathRoot = "/var/log/solarmonj/";
-	
-	if ( !conn.init() ) {
-		cerr << "Cannot initialise the connection." << endl;
-		return 1;
-	}
-	
-	// Get the current time
-	time_t now = time(0);
-	
-	// Get the data
-	Jfy::InverterData data;
-	conn.readNormalInfo( &data );
-	
-	// Write to logstash
+// Date format has requested by PVOutPut
+const std::string currentDate() {
+time_t now = time(0);
+struct tm tstruct;
+char buf[80];
+tstruct = *localtime(&now);
+strftime(buf, sizeof(buf), "%Y%m%d", &tstruct);
 
-	try
-	{
-		ClientSocket client_socket ( "localhost", 7022 );
-
-		try
-		{
-			stringstream ss;
-
-			ss << now << "," <<   data.temperature << "," << data.energyCurrent << "," << data.energyToday << "," << data.pvoltageAc << "," << data.voltageDc << "," << data.voltageAc << "," << data.frequency << "," << data.current << "\n";
-
-			client_socket << ss.str();
-			std::cout << "Sent\n";
-		}
-		catch (SocketException& e)
-		{
-      			std::cout << "Error writing to logging server:\n\"" << e.description() << "\"\n";	
-		}
-
-	} 
-	catch (SocketException& e)
-	{
-		std::cout << "Error connecting to logging server:\n\"" << e.description() << "\"\n";
-	}
-
-	conn.close();
-
-	return 0;
+return buf;
 }
 
+// Time format has requested by PVOutPut
+const std::string currentTime() {
+time_t now = time(0);
+struct tm tstruct;
+char buf[80];
+tstruct = *localtime(&now);
+strftime(buf, sizeof(buf), "%R", &tstruct);
+
+return buf;
+}
+
+int main( int argc, char** argv )
+{
+
+Jfy::Connection conn( "/dev/serial0" );
+string APIKey = "API Key";
+string systemID = "SYSTEM ID";
+
+if ( !conn.init() ) {
+cerr << "Cannot initialise the connection." << endl;
+return 1;
+}
+
+// Get the data
+Jfy::InverterData data;
+conn.readNormalInfo( &data );
+
+std::ostringstream curr_val_str;
+curr_val_str << "curl -d \"d=" << currentDate() << 
+"\" -d \"t=" << currentTime() <<
+"\" -d \"v1=" << data.frequency <<
+"\" -d \"v2=" << data.energyToday <<
+"\" -d \"v5=" << data.temperature <<
+"\" -d \"v6=" << data.voltageDc <<
+"\" -H \"X-Pvoutput-Apikey: " << APIKey <<
+"\" -H \"X-Pvoutput-SystemId: " << systemID << 
+"\" http://pvoutput.org/service/r2/addstatus.jsp";
+
+std::cout << curr_val_str.str().c_str();
+
+// Some times the inverter returns no data by the 1st attempt, just a quick check and launch a 2nd attempt if it failed
+if ((int)data.temperature != 0) 
+{
+system(curr_val_str.str().c_str());
+// Close the connection to the inverter. 
+conn.close();
+} else {
+Jfy::InverterData data;
+conn.readNormalInfo( &data );
+
+std::ostringstream curr_val_str;
+curr_val_str << "curl -d \"d=" << currentDate() << 
+"\" -d \"t=" << currentTime() <<
+"\" -d \"v1=" << data.frequency <<
+"\" -d \"v2=" << data.energyToday <<
+"\" -d \"v5=" << data.temperature <<
+"\" -d \"v6=" << data.voltageDc <<
+"\" -H \"X-Pvoutput-Apikey: " << APIKey <<
+"\" -H \"X-Pvoutput-SystemId: " << systemID << 
+"\" http://pvoutput.org/service/r2/addstatus.jsp";
+
+std::cout << curr_val_str.str().c_str();	
+if ((int)data.temperature != 0) 
+{
+system(curr_val_str.str().c_str());
+}	
+// Close the connection to the inverter. 
+conn.close();
+}
+
+return 0;
+}
